0f7ae10f5237871ce8f891b445341296b1d2e489
Chris Hopkins <cbehopkins@gmail.com>
2016-09-02 21:21:44 +0000

    Initial checkin - pings self

--- base_test.go
+++ base_test.go
@@ -10,11 +10,27 @@ func TestConn(t *testing.T) {
 	log.Println("Starting Client...")
 
 	conn := NewClient()
+	defer conn.Close()
 	//////////
 	// Now send to an open connection
 	err := SendPing(conn)
 	check(err)
+	// Send 2 pings to make sure the connection can do this
 	err = SendPing(conn)
 	check(err)
-	conn.Close()
+}
+
+func TestParrallel(t *testing.T) {
+
+	log.Println("Starting Client...")
+
+	conn := NewClient()
+	defer conn.Close()
+	// In ths test we will start up a side TCP channel to check we can send and receive chunks of data
+	bconn, err := NewBulkConn(conn)
+	check(err)
+	defer bconn.Close()
+	err = SendRxBulk(1000, conn)
+	check(err)
+
 }
--- client.go
+++ client.go
@@ -38,3 +38,37 @@ func NewClient() (conn net.Conn) {
 	}
 	return
 }
+func NewBulkConn(iconn net.Conn) (oconn net.Conn, err error) {
+	bob := Message{Action: "BConn", Data: "8085"}
+	snd_mess, err := MarshalMessage(bob)
+	if err != nil {
+		log.Fatal("Error marshalling", err)
+		return
+	}
+	fmt.Fprintln(conn, snd_mess)
+	//////////
+	// listen for reply on open connection
+	message, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Printf("Read String error: %v\n", err)
+	}
+	//log.Println("Received Message:", message)
+	item, err := UnmarshalMessage(message)
+	check(err)
+	if item.Action != "Bonn" {
+		err = fmt.Errorf("Bulk Connection is not Bonn:%s", message)
+		return
+	}
+	// Now we have created a listener for it, open the bulk connection
+	oconn, err = net.Dial("tcp", "127.0.0.1:8085")
+	check(err)
+	return
+
+}
+func SendRxBulk(count int, conn net.Conn) error {
+	data_2_send := make([]byte, count)
+	fmt.Fprintln(conn, data_2_send)
+	_, err := bufio.NewReader(conn).ReadString('\n')
+	check(err)
+	return err
+}
