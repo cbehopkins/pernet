9cc5f03f53bbe10e86d45df33469f64bbdafa87f
Chris Hopkins <cbehopkins@gmail.com>
2016-09-08 10:40:09 +0000

    Minor tidies

--- base_test.go
+++ base_test.go
@@ -49,15 +49,19 @@ func TestBatch(t *testing.T) {
 	//	}
 
 }
-func TestParrallel(t *testing.T) {
+func runTestNet (num_connections int, len_p uint, t *testing.B) {
+	num_its := 1
+	if t != nil {
+		num_its = t.N
+	} 
+	length := 1<< len_p
 
 	log.Println("Starting Client...")
-
 	conn := NewClient()
 	defer conn.CloseAll()
 	open_cons := make(map[int]struct{})
 	// In ths test we will start up a side TCP channel to check we can send and receive chunks of data
-	for i := 0; i < 16; i++ {
+	for i := 0; i < num_connections; i++ {
 		port_num, err := conn.NewBulkConn()
 		check(err)
 		log.Println("Open connection on port:", port_num)
@@ -66,18 +70,32 @@ func TestParrallel(t *testing.T) {
 	log.Println("Bulk connections opened, try to send something")
 
 	var out_count sync.WaitGroup
-	out_count.Add(16)
+	if t != nil {
+		t.ResetTimer()
+	}
+
+	for i:=0;i<num_its;i++ {
+	out_count.Add(num_connections)
 	for port_num := range open_cons {
 		go func(pn int ) {
-			err := conn.SendRxBulk(1000, pn)
+			err := conn.SendRxBulk(length, pn)
 			check(err)
 			out_count.Done()
 		}(port_num)
 	}
 	out_count.Wait()
+	}
 	for i := range open_cons {
 		err := conn.CloseBulkConn(i)
 		check(err)
 	}
+}
 
+func TestParrallel(t *testing.T) {
+	runTestNet(16, 10, nil)
+}
+
+
+func BenchmarkNet(b *testing.B)  {
+	runTestNet(16, 10, b)
 }
